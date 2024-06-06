34993c96c549ba1fc4be0cd0177af215e0753c66
developer3A <abduqayumovakbarali2@gmail.com>
2024-06-07 01:52:23 +0500

    Add files via upload

---
+++ eco bot_for_deploy/data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = '7027881218:AAHSSTeDZOnUDedW926WGZoC0R78ui2wF_s'
+ADMINS = 5000611479
+IP = "localhost"
